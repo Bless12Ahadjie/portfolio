babe0ed440bc7639aa96e3dcf6620392ee3891db
bless12ahadjie <blaise0ahadjie@gmail.com>
2023-10-17 13:11:39 +0000

    Updated Color schemes and changed spacing

--- app/components/molecules/HeroLeft/Hero-Left.tsx
+++ app/components/molecules/HeroLeft/Hero-Left.tsx
@@ -3,12 +3,15 @@ import './left.css';
 
 
 const HeroLeft = () => {
+    const btnStyle ={
+        marginTop: "2rem"
+    }
   return(
     <main className="LeftCointainer">
-        <span className="name-span"> Bless Ahadjie /></span>
+        <span className="name-span"> Bless Ahadjie /> </span>
         <p className="second-txt">I Create <span className="stack">Front-End</span> Web Applications</p>
-        <p className=".last-txt">My name is  and I am a Front-end Dev.</p>
-        <Button className="btn" name = "Contact Me"/>
+        <p className=".last-txt">As a forward-thinking creator, I am actively sculpting the digital terrain. My expertise lies in breathing life into captivating Front-End Web Applications, where each click narrates a unique tale</p>
+        <Button  style={btnStyle}className="btn" name = "Contact Me"/>
     </main>
   ) ;
 };
